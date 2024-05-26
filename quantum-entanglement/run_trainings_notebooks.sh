287ee056a58dcd23967d949c3f02a0f413991b08
Faical Jouini <fjouini@gmail.com>
2024-05-27 01:45:35 +0200

    Clean up

--- quantum-entanglement/run_trainings_notebooks.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-papermill 01-Training.ipynb ./executed_notebooks/01-Training-CHSH.ipynb -p WITNESS_NAME "CHSH"
-papermill 01-Training.ipynb ./executed_notebooks/01-Training-CONCURRENCE.ipynb -p WITNESS_NAME "CONCURRENCE"
-papermill 01-Training.ipynb ./executed_notebooks/01-Training-ENTROPY.ipynb -p WITNESS_NAME "ENTROPY"
-papermill 01-Training.ipynb ./executed_notebooks/01-Training-NEGATIVITY.ipynb -p WITNESS_NAME "NEGATIVITY"
