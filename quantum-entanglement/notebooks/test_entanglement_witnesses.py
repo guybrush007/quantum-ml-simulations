422443c4dd64f3e972577233f2f810315e0d5ce2
Faical Jouini <fjouini@gmail.com>
2024-06-08 13:26:25 +0200

    Add input example and werner state tests

--- quantum-entanglement/notebooks/test_entanglement_witnesses.py
+++ quantum-entanglement/notebooks/test_entanglement_witnesses.py
@@ -1,6 +1,7 @@
 from entanglement_witnesses import witnesses
+from simulation_utils import create_bell_states
 import pytest
-from qutip import basis, tensor, rand_ket
+from qutip import basis, tensor, rand_ket, qeye
 import numpy as np
 
 def  create_bell_pure_state():
@@ -28,15 +29,32 @@ def create_mixed_not_entangled_state_dm():
     not_entangled_2 = tensor(rand_ket(2), rand_ket(2)).unit()
     return (0.5 * not_entangled_1 * not_entangled_1.dag() + 0.5 * not_entangled_2 * not_entangled_2.dag())
 
+bell_states = create_bell_states()
+phi_plus = bell_states[0]
+phi_minus = bell_states[1]
+psi_plus = bell_states[2]
+psi_minus = bell_states[3]
+
+def create_werner_state_dm():
+    p = 0.3 + 0.7 * np.random.random()
+    print(f"psi_minus {psi_minus}")
+    return p * psi_minus + (1-p) * qeye(dimensions=[[2, 2]]) / 4
+
 
 @pytest.mark.parametrize("witness", witnesses.values())
-def test_is_entangled_chsh_bell_state_pure_state(witness):
+def test_is_entangled_werner_state_dm(witness):
+    state = create_werner_state_dm()
+    is_entangled = witness(state)
+    assert is_entangled is True
+
+@pytest.mark.parametrize("witness", witnesses.values())
+def test_is_entangled_bell_state_pure_state(witness):
     state = create_bell_pure_state()
     is_entangled = witness(state)
     assert is_entangled is True
 
 @pytest.mark.parametrize("witness", witnesses.values())
-def test_is_entangled_chsh_bell_state_pure_state_dm(witness):
+def test_is_entangled_bell_state_pure_state_dm(witness):
     state = create_bell_pure_state_dm()
     is_entangled = witness(state)
     assert is_entangled is True
